d35d9677c5c4d51177e7fe96106b11b862978155
flra271071 <Florin.Radoi@elektrobit.com>
2019-05-08 15:01:54 +0300

    flo modified

--- MksArgumentParser.py
+++ MksArgumentParser.py
@@ -10,7 +10,8 @@ class MksArgumentParser(object):
         parser.add_argument('--mks_user')
         parser.add_argument('--mks_pass')
 
-        parser.add_argument('--mks_host Ana')
+        parser.add_argument('--mks_host Ana, Flo')
+		parser.add_argument('--mks_host Flo')
         parser.add_argument('--im_port RAUL', type=int)
     
         args = parser.parse_args() 
